b89ae4a4d00011f3ec5c8d1466ec094ab744b3e9
Jeronimo Quintero Gomez <jeronimoq18@gmail.com>
2025-04-24 12:18:45 -0500

    Carga de linea base

---
+++ src/main/java/co/com/munamu/munamuinventory/initializer/MunamuinventoryApplication.java
@@ -0,0 +1,13 @@
+package co.com.munamu.munamuinventory.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunamuinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunamuinventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/munamu/munamuinventory/initializer/MunamuinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.munamu.munamuinventory.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunamuinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
